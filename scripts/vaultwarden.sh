663f9fe6da30a44ead9e730ce44d715bba069de0
Zeuxis Lo <seekstudio@gmail.com>
2023-04-19 12:12:32 +0800

    ask for input when generate vaultwarden admin token

--- scripts/vaultwarden.sh
+++ scripts/vaultwarden.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-PASSWORD="YOUR_PASSWORD_HERE";
+# PASSWORD="YOUR_PASSWORD_HERE";
+echo -n Password:
+read -s password
+echo
 
-echo -n $PASSWORD | argon2 "$(openssl rand -base64 32)" -e -id -k 65540 -t 3 -p 4
+echo -n $password | argon2 "$(openssl rand -base64 32)" -e -id -k 65540 -t 3 -p 4
